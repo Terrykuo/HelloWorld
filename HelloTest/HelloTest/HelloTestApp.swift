fe7bcbd8a1a44d90527bc86ea15645dee28f200c
Terry Kuo <terrykuo@gmail.com>
2023-03-16 15:44:39 +0800

    Creat Project

---
+++ HelloTest/HelloTest/HelloTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTestApp.swift
+//  HelloTest
+//
+//  Created by TerryKuo on 2023/3/16.
+//
+
+import SwiftUI
+
+@main
+struct HelloTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
